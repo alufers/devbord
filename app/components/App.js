a9a0be3184c1b6c26a2fc7d8242be8540ab7c7b4
xdk78 <xdk78@interia.pl>
2018-04-28 18:59:13 +0200

    Add placeholder menu

--- app/components/App.js
+++ app/components/App.js
@@ -2,17 +2,32 @@ import React from "react";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import AppBar from "material-ui/AppBar";
 import styled from "styled-components";
+import { Link } from "../routes";
+import MenuItem from 'material-ui/MenuItem';
 
 const Container = styled.div`
   width: 70%;
   margin: 0 auto;
 `;
 
-export default ({ children }) => (
-  <MuiThemeProvider>
-    <div>
-      <AppBar title="DevBord" />
-      <Container>{children}</Container>
-    </div>
-  </MuiThemeProvider>
-);
+export default class App extends React.Component {
+  render() {
+    const {children} = this.props;
+    return (
+    <MuiThemeProvider>
+      <div>
+        <AppBar 
+            title="DevBord" 
+            iconElementRight={
+              <Link route='/'>
+                <MenuItem primaryText="Home" />
+              </Link>
+        }
+        >
+        </AppBar>
+        <Container>{children}</Container>
+      </div>
+    </MuiThemeProvider>
+    );
+  }
+}
