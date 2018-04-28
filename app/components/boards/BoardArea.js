5e358c74c33bc907a0de0f53d173d83a6693e778
alufers <alufers@wp.pl>
2018-04-28 17:27:04 +0200

    Added board renamer

--- app/components/boards/BoardArea.js
+++ app/components/boards/BoardArea.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Paper from "material-ui/Paper";
 import styled from "styled-components";
+import EditableName from "../common/EditableName";
 
 const AreaName = styled.div`
   margin-bottom: 5px;
@@ -10,11 +11,13 @@ const BoardAreaPaper = styled(Paper)`
   margin-right: 20px;
   padding: 10px;
   min-height: 400px;
-  min-width: 250px;
+  min-width: 300px;
 `;
 
 export default ({ area }) => (
   <BoardAreaPaper>
-    <AreaName>{area.name}</AreaName>
+    <AreaName>
+      <EditableName value={area.name} />
+    </AreaName>
   </BoardAreaPaper>
 );
