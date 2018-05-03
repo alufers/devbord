d3a3e38cc374d11b6bef901436134ea5d12c6e5d
alufers <alufers@wp.pl>
2018-05-03 12:32:25 +0200

    Added renaming of boards and board areas

---
+++ app/components/EditableName.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <v-text-field ref="inp" :value="internalValue" @input="internalValue = $event" @blur="emitValue" @keyup.enter="$refs.inp.blur()" @keyup.esc="discardValue"></v-text-field>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      internalValue: ""
+    };
+  },
+  watch: {
+    value(newVal) {
+      this.internalValue = newVal;
+    }
+  },
+  methods: {
+    emitValue() {
+      this.$emit("input", this.internalValue);
+    },
+    discardValue() {
+      this.internalValue = this.value;
+      this.$refs.inp.blur();
+    }
+  },
+  created() {
+    this.internalValue = this.value;
+  },
+  props: {
+    value: {
+      type: String,
+      default: ""
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
--- app/pages/board/_id.vue
+++ app/pages/board/_id.vue
@@ -3,12 +3,16 @@
     <v-progress-circular indeterminate :size="50" color="primary"></v-progress-circular>
   </div>
   <div v-else>
-    <h1 class="heading">{{board && board.name}}</h1>
+    <h1 class="heading">
+      <EditableName :value="board.name" @input="renameBoard($event)" />
+    </h1>
     <div class="board-areas">
       <v-card v-for="area in board.areas" :key="area.id" class="mr-3 area elevation-1">
         <v-card-title primary-title>
           <div>
-            <h3 class="title mb-0">{{area.name}}</h3>
+            <h3 class="title mb-0">
+              <EditableName :value="area.name" @input="renameBoardArea(area.id, $event)" />
+            </h3>
           </div>
         </v-card-title>
 
@@ -23,12 +27,51 @@
 
 <script>
 import gql from "graphql-tag";
+import EditableName from "../../components/EditableName";
+
 export default {
+  components: {
+    EditableName
+  },
   async asyncData({ params }) {
     return {
       id: params.id,
       board: null
     };
+  },
+  methods: {
+    async renameBoard(newName) {
+      await this.$apollo.mutate({
+        mutation: gql`
+          mutation renameBoard($boardId: ID!, $newName: String!) {
+            updateBoard(where: { id: $boardId }, data: { name: $newName }) {
+              id
+              name
+            }
+          }
+        `,
+        variables: {
+          boardId: this.id,
+          newName
+        }
+      });
+    },
+    async renameBoardArea(areaId, newName) {
+      await this.$apollo.mutate({
+        mutation: gql`
+          mutation renameBoardArea($areaId: ID!, $newName: String!) {
+            updateBoardArea(where: { id: $areaId }, data: { name: $newName }) {
+              id
+              name
+            }
+          }
+        `,
+        variables: {
+          areaId,
+          newName
+        }
+      });
+    }
   },
   apollo: {
     board: {
